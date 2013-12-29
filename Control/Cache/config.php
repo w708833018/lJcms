84aeda2aa67f0f3f06c75192aab566b4e2d47fb7
wanglei <708833018@qq.com>
2013-12-29 19:54:24 +0800

    配置

---
+++ Control/Cache/config.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+		'DB_HOST'=>'localhost',
+		'DB_NAME'=>'fly',
+		'DB_USER'=>'root',
+		'DB_PWD'=>'wanglei',
+		'DB_PREFIX'=>'gz_',
+	);
+?>
---
+++ Control/Cache/longji.php
@@ -0,0 +1,1 @@
+<?php	return array ( 'SAFE_CODE' => 'InGVSoWePEdaFfTiTrLjkmhGB', 'HTML_ROOT' => '/html/', 'BACK_PATH' => 'DataBackup', 'HTML_EXT' => '.html', 'MENU_CONTENT' => '13', 'MENU_GUESTBOOK' => '12', 'PAGE_LISTROWS' => '6', 'PAGE_ROWS' => '12', 'DEFAULT_LANG' => 'cn', 'IS_HTML' => '0', );?>
